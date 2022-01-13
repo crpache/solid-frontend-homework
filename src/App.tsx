ed0c4785f863c6ea1f016fc2b24e1fa21dac7cf7
cristian pache <crpache@gmail.com>
2022-01-13 16:17:02 -0300

    add network validation

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,53 @@ declare let window: any;
 
 const App = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isMetamaskNotInstalled, setIsMetamasNotInstalled] = useState(false);
+  const [isMetamaskNotInstalled, setIsMetamaskNotInstalled] = useState(false);
   const [isMetamaskConnected, setIsMetamaskConnected] = useState(false);
   const [accountBalance, setAccountBalance] = useState("");
   const [isInvalidAddress, setIsInvalidAddress] = useState(false);
-  const [contract, setContract] = useState<ethers.Contract>()
+  const [network, setNetwork] = useState("");
+  const [contract, setContract] = useState<ethers.Contract>();
 
   useEffect(() => {
     if (window.ethereum && window.ethereum.isMetaMask) {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      setContract(new ethers.Contract(ADDRESS, ABI, provider));
-      setIsMetamasNotInstalled(false)
-    } else {
-      setIsMetamasNotInstalled(true)
+      window.ethereum.on("chainChanged", (chainId: string) => {
+        setNetwork(chainId); // set network value every time network changes
+      });
     }
-  }, [])
-  
+  }, []);
+
+  useEffect(() => {
+    if (window.ethereum && window.ethereum.isMetaMask) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      setContract(new ethers.Contract(ADDRESS, ABI, provider));
+      setIsMetamaskNotInstalled(false);
+    } else {
+      setIsMetamaskNotInstalled(true);
+    }
+  }, [network]);
+
   const handleConnectToWallet = async () => {
     try {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      setIsMetamaskConnected(accounts.length > 0);
-    } catch (err) {
-      setIsMetamaskConnected(false);
+      await window.ethereum.request({
+        method: "eth_requestAccounts", // request connection
+      });
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain", // request use Rinkeby network
+        params: [{ chainId: "0x4" }],
+      });
+      setIsMetamaskConnected(true);
+    } catch (switchError: any) {
+      if (switchError.code === 4902) {
+        try {
+          await window.ethereum.request({
+            method: "wallet_addEthereumChain", // request add Rinkeby network if is not added
+            params: [{ chainId: "0x4" }],
+          });
+          setIsMetamaskConnected(true);
+        } catch (addError) {
+          setIsMetamaskConnected(false);
+        }
+      }
     }
   };
 
@@ -51,7 +76,7 @@ const App = () => {
     setIsModalOpen(false);
     setAccountBalance("");
     setIsInvalidAddress(false);
-  }
+  };
 
   return (
     <>
